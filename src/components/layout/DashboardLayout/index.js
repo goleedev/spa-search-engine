3868be0691fdc87405bd6dde969da9505f187236
GO Lee <golee.dev@gmail.com>
2024-10-10 19:24:15 +0100

    refactor: update DashboardLayout component to include Sidebar and UserProfile

--- src/components/layout/DashboardLayout/index.js
+++ src/components/layout/DashboardLayout/index.js
@@ -1,7 +1,19 @@
+import Sidebar from "../Sidebar";
+import UserProfile from "../UserProfile";
 import * as S from "./styles";
 
-const DashboardLayout = ({ children }) => {
-  return <S.DashboardLayout>{children}</S.DashboardLayout>;
-};
+const DashboardLayout = ({ children }) => (
+  <S.DashboardLayout>
+    <Sidebar />
+
+    <S.MainContent>
+      <S.Header>
+        <UserProfile name="Name Surname" initials="CC" />
+      </S.Header>
+
+      {children}
+    </S.MainContent>
+  </S.DashboardLayout>
+);
 
 export default DashboardLayout;
--- src/components/layout/DashboardLayout/styles.js
+++ src/components/layout/DashboardLayout/styles.js
@@ -1,3 +1,21 @@
 import styled from "styled-components";
 
-export const DashboardLayout = styled.div``;
+export const DashboardLayout = styled.div`
+  display: flex;
+  height: 100%;
+`;
+
+export const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 24px 150px 130px 150px;
+  margin-left: 100px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  padding: 10px 0;
+s`;
