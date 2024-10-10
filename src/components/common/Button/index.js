3e967779f80edb57244cfc0d18f5de409aeaa2ea
GO Lee <golee.dev@gmail.com>
2024-10-10 21:06:43 +0100

    refactor: update Button component styles

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,7 +1,9 @@
 import * as S from "./styles";
 
-const Button = () => {
-  return <button />;
-};
+const Button = ({ children, onClick, width }) => (
+  <S.Button onClick={onClick} width={width}>
+    {children}
+  </S.Button>
+);
 
 export default Button;
--- src/components/common/Button/styles.js
+++ src/components/common/Button/styles.js
@@ -1,1 +1,21 @@
 import styled from "styled-components";
+
+import theme from "../../../styles/theme";
+
+export const Button = styled.button`
+  padding: 14px 0;
+  width: ${({ width }) => width};
+  height: 48px;
+  text-align: center;
+  border-radius: ${theme.spacing.xs};
+  cursor: pointer;
+  border: none;
+
+  background-color: ${theme.colors.primary};
+  color: ${theme.colors.white};
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: ${theme.colors.primaryDark};
+  }
+`;
