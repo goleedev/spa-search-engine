ea0c3bd15551efe3426156c77c6e94b30e017ea0
GO Lee <golee.dev@gmail.com>
2024-10-10 22:03:33 +0100

    refactor: update InputField component styles and functionality

--- src/components/common/InputFIeld/index.js
+++ src/components/common/InputFIeld/index.js
@@ -1,7 +1,12 @@
 import * as S from "./styles";
 
-const InputField = () => {
-  return <input />;
+const InputField = ({ value, onChange, placeholder = "Search", icon: Icon, width }) => {
+  return (
+    <S.InputWrapper>
+      {Icon && <Icon />}
+      <S.InputField value={value} onChange={onChange} placeholder={placeholder} width={width} />
+    </S.InputWrapper>
+  );
 };
 
 export default InputField;
--- src/components/common/InputFIeld/styles.js
+++ src/components/common/InputFIeld/styles.js
@@ -1,1 +1,36 @@
 import styled from "styled-components";
+
+import theme from "../../../styles/theme";
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid ${theme.colors.grey.light};
+  border-radius: ${theme.spacing.xs};
+  padding: 14px ${theme.spacing.md};
+  width: ${({ width }) => width || "100%"};
+  height: 48px;
+  max-width: 600px;
+
+  svg {
+    margin-right: ${theme.spacing.xs};
+    width: 20px;
+    height: 20px;
+    stroke: ${theme.colors.grey.darker};
+  }
+`;
+
+export const InputField = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  font-size: ${theme.font.size.xs};
+  line-height: ${theme.font.size.md};
+
+  color: ${theme.colors.textPrimary};
+  background-color: transparent;
+
+  &::placeholder {
+    color: ${theme.colors.grey.darkest};
+  }
+`;
