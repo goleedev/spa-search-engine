65782493568e8215b2d4948f570ea8ffe9771efd
GO Lee <golee.dev@gmail.com>
2024-10-10 22:07:26 +0100

    feat: add SearchForm component

---
+++ src/components/features/SearchForm/index.js
@@ -0,0 +1,22 @@
+import { SearchIcon } from "../../../assets";
+import Button from "../../common/Button";
+import InputField from "../../common/InputField";
+import * as S from "./styles";
+
+const SearchForm = ({ query, setQuery, onSearch }) => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSearch();
+  };
+
+  return (
+    <S.SearchForm onSubmit={handleSubmit}>
+      <InputField placeholder="Search" value={query} onChange={e => setQuery(e.target.value)} icon={SearchIcon} />
+      <Button type="submit" width="192px">
+        Search
+      </Button>
+    </S.SearchForm>
+  );
+};
+
+export default SearchForm;
---
+++ src/components/features/SearchForm/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+import theme from "../../../styles/theme";
+
+export const SearchForm = styled.form`
+  display: flex;
+  gap: ${theme.spacing.md};
+  padding-bottom: 32px;
+`;
