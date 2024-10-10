82c555094d4e2e0ecd84a89037490302ce173331
GO Lee <golee.dev@gmail.com>
2024-10-10 22:27:43 +0100

    refactor: update Dropdown component styles and functionality

--- src/components/common/Dropdown/index.js
+++ src/components/common/Dropdown/index.js
@@ -1,8 +1,35 @@
 import Select from "react-select";
+
+import theme from "../../../styles/theme";
 import * as S from "./styles";
 
-const Dropdown = () => {
-  return <Select />;
+const customSelectStyles = {
+  control: provided => ({
+    ...provided,
+    borderRadius: theme.spacing.xs,
+    height: "48px",
+    border: `1px solid ${theme.colors.grey.light}`,
+    boxShadow: "none",
+    cursor: "pointer",
+  }),
+  placeholder: provided => ({
+    ...provided,
+    color: theme.colors.grey.darkest,
+    fontSize: theme.font.size.xs,
+  }),
 };
+
+const Dropdown = ({ options, placeholder, value, onChange }) => (
+  <S.SelectWrapper>
+    <Select
+      styles={customSelectStyles}
+      placeholder={placeholder}
+      options={options}
+      value={value}
+      onChange={onChange}
+      components={{ IndicatorSeparator: () => null }}
+    />
+  </S.SelectWrapper>
+);
 
 export default Dropdown;
--- src/components/common/Dropdown/styles.js
+++ src/components/common/Dropdown/styles.js
@@ -1,1 +1,6 @@
 import styled from "styled-components";
+
+export const SelectWrapper = styled.div`
+  width: 100%;
+  max-width: 400px;
+`;
