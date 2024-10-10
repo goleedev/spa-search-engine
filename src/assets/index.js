a49253a65c48dc8cf772472d47276252d11c0120
GO Lee <golee.dev@gmail.com>
2024-10-10 18:31:39 +0100

    feat: add new icons

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+export { ReactComponent as ContactsIcon } from "./icons/contacts.svg";
+export { ReactComponent as FacebookIcon } from "./icons/facebook.svg";
+export { ReactComponent as FilesIcon } from "./icons/files.svg";
+export { ReactComponent as HomeIcon } from "./icons/home.svg";
+export { ReactComponent as InfoIcon } from "./icons/info.svg";
+export { ReactComponent as InstagramIcon } from "./icons/instagram.svg";
+export { ReactComponent as LinkedinIcon } from "./icons/linkedin.svg";
+export { ReactComponent as SearchIcon } from "./icons/search.svg";
+export { ReactComponent as SettingIcon } from "./icons/setting.svg";
+export { ReactComponent as SuitcaseIcon } from "./icons/suitcase.svg";
+export { ReactComponent as TiktokIcon } from "./icons/tiktok.svg";
+export { ReactComponent as TwitterIcon } from "./icons/twitter.svg";
+export { ReactComponent as YoutubeIcon } from "./icons/youtube.svg";
